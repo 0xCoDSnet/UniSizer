a5ca66cca21145994ec21a9a96f79b5ca3edc6d0
0xCoDSnet <76873593+0xCoDSnet@users.noreply.github.com>
2022-11-26 15:35:23 +0400

    Update Enums.py

--- Enums.py
+++ Enums.py
@@ -8,7 +8,7 @@ from enum import Enum
 class SchemeTransformation(Enum):
     # ru -> en -> nl -> en -> ru
     Scheme_1_to_reverso = "en,nl,en"
-
+    Scheme_2_to_reverso = "en,nl,tr,en"
 
 class Translators(Enum):
     google = "google"
